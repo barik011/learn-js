d79c9213316adbbfbb223d56612d00db781b51de
Github Projects <barik011@gmail.com>
2022-03-23 08:00:43 +0530

    https://github.com/barik011/learn-js

---
+++ 01/variable.js
@@ -0,0 +1,16 @@
+var fullName="Mohammad Barique";
+
+console.log(fullName);
+
+var  errorMsg="You are not login, Please login here";
+
+console.log(errorMsg);
+
+var isLogIn = true;
+
+if(isLogIn){
+    console.log("Hi "+fullName+" Welcome to JS Classess");
+}
+else{
+    console.log(errorMsg);
+}
