0ba44a558d4246a316130eddf1d66da83a2df773
Github Projects <barik011@gmail.com>
2022-03-24 09:02:42 +0530

    2nd day class of variable

---
+++ 01/02variable.js
@@ -0,0 +1,16 @@
+const uid="abc123";
+// uid="abcd456";
+console.log(uid);
+
+var fullName="Barique Quamar";
+var email="barik011@gmail.com";
+var passowrd=123321;
+var confPassword=123321;
+var courseCount=50;
+var isLoogedIn=false;
+
+console.log("Full Name: ",fullName);
+console.log("Email Id: ",email);
+console.log("Password: ",passowrd);
+console.log("Course Count: ",courseCount);
+console.log("Log In Status: ",isLoogedIn);
