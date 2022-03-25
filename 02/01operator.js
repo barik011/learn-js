31b7e1e56590c164e9a3524df9bc7099073b9157
Github Projects <barik011@gmail.com>
2022-03-25 09:14:15 +0530

    Operator Classess

--- 02/01operator.js
+++ 02/01operator.js
@@ -1,1 +1,11 @@
 console.log("Operator Classess")
+
+var num1=10;
+var nam2=20;
+
+
+console.log("Addition of num1 + num2", num1 + num2);
+
+console.log("Substraction of num2 - num1", num2 - num1);
+
+console.log("Multiplication of num1 * num2", num1 * num2);
