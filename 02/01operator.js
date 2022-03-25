0e051000f54ef22786015432e04bcacece493e0d
Github Projects <barik011@gmail.com>
2022-03-25 09:16:57 +0530

    same

--- 02/01operator.js
+++ 02/01operator.js
@@ -4,8 +4,12 @@ var num1=10;
 var nam2=20;
 
 
-console.log("Addition of num1 + num2", num1 + num2);
+console.log("Addition of num1 + num2 :", num1 + num2);
 
-console.log("Substraction of num2 - num1", num2 - num1);
+console.log("Substraction of num2 - num1 :", num2 - num1);
 
-console.log("Multiplication of num1 * num2", num1 * num2);
+console.log("Multiplication of num1 * num2 :", num1 * num2);
+
+var result = num2 > num1;
+
+console.log("num2 is greater then num1:  ", num1 * num2);
