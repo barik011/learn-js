fd5da49744fd33d2e33b5136ec9226f3cc1dd90f
Github Projects <barik011@gmail.com>
2022-03-30 09:34:35 +0530

    operator type

--- 02/02operator.js
+++ 02/02operator.js
@@ -6,3 +6,9 @@ console.log("Without Round Figure: "+ discountPrice);
 
 console.log("In Round Figure: "+ Math.round(discountPrice)+ "% off");
 
+
+console.log(typeof listPrice);
+
+var result = listPrice>sellPrice;
+
+console.log(typeof result);
