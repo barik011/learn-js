54121fb74b55033d4b95302c0996f1efa928692e
Github Projects <barik011@gmail.com>
2022-03-30 10:12:43 +0530

    Operator Type

--- 02/02operator.js
+++ 02/02operator.js
@@ -12,3 +12,17 @@ console.log(typeof listPrice);
 var result = listPrice>sellPrice;
 
 console.log(typeof result);
+
+var num1 = 2;
+var num2 = 3;
+var num3 = 4;
+var num4 = 5;
+
+var outPut = num1*num2;
+console.log( "num1*num2 :" + outPut);
+
+var outPut = num1**num2;
+console.log( "num1*num2 :" + outPut);
+
+var outPut = num1*num2**num3;
+console.log( "num1*num2 :" + outPut);
