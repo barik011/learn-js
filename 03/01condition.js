02b58a13178d675a2a8283c7bb95794e3dd21bfd
Github Projects <barik011@gmail.com>
2022-04-01 09:40:45 +0530

    condition practice1

---
+++ 03/01condition.js
@@ -0,0 +1,11 @@
+var temperature = 29;
+
+if(temperature<20){
+    console.log("Cold outside");
+}
+else if(temperature<30){
+    console.log("Modrate outside");
+}
+else{
+    console.log("Hot outside");
+}
