5b5d414a0df89c380e1708130d4f65a782b5c186
Github Projects <barik011@gmail.com>
2022-04-04 09:49:44 +0530

    Bitwise Operator

--- 03/02condition.js
+++ 03/02condition.js
@@ -1,4 +1,9 @@
-var gmail =true;
+var gmail =false;
 var facebook = false;
 var google= false;
 
+if(gmail||facebook||google){
+    console.log("Loggedin successfully");
+}else{
+    console.log("Login Failure");
+}
---
+++ 04/01authontication.js
@@ -0,0 +1,10 @@
+var authenticated=true;
+
+// if(authenticated){
+//     console.log("Show Logout");
+// }
+// else{
+//     console.log("Show Login");
+// }
+
+authenticated ? console.log("Show Logout"):console.log("Show Login");
