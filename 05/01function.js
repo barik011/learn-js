93ed242b1822a99428db4eee1174d3dfafbcf0d3
Github Projects <barik011@gmail.com>
2022-04-05 09:55:07 +0530

    basic functions

---
+++ 05/01function.js
@@ -0,0 +1,14 @@
+function sayHi(name){
+    console.log(`Hello ${name}`);
+}
+
+sayHi('Barique');
+sayHi("Mohammad Barique");
+
+function greeting(){
+    return "Welcome to Home";
+}
+
+var gretValue = greeting();
+
+console.log(gretValue);
