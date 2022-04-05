ff24696ebd01e7b9c780a8a5c03221315f76cc1d
Github Projects <barik011@gmail.com>
2022-04-05 09:14:34 +0530

    witout break

--- 04/01switch.js
+++ 04/01switch.js
@@ -7,16 +7,16 @@ var user="trail"
 switch (user) {
     case "superAdmin":
         console.log("Access all thing"); 
-        break;
+    
 
     case "admin":
         console.log("Access all thing except create user");
-        break;
+
     case "subAdmin":
         console.log("Access view,update user");
-        break;
+  
 
     default:console.log("View for trial user");
-        break;
+
 }
 
