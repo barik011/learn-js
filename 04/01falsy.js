d299053e6a4e148ffbc3cb7249c1963b01abb89b
Github Projects <barik011@gmail.com>
2022-04-05 09:21:47 +0530

    Undife-Nan-Null

---
+++ 04/01falsy.js
@@ -0,0 +1,20 @@
+// undefine
+// NaN
+// ""
+// 0
+
+var a;
+
+console.log(a);
+
+var b=2
+
+if(b=="2"){
+    console.log("Only value checked")
+}
+if(b==="2"){
+    console.log("Value and Type Matched")
+}
+else{
+    console.log("Value and Type Not Matched")
+}
