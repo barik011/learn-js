0241c3c88fd06bdcea71c878b77ac5b5a3a70424
Github Projects <barik011@gmail.com>
2022-04-11 09:30:42 +0530

    array

--- 06/01array.js
+++ 06/01array.js
@@ -2,6 +2,10 @@ var country = ["India", "Russia","Nepal","Srilanka","Bangladesh"];
 
 var state = new Array("Bihar","Delhi","UP","Jharkhand");
 
-console.log(country);
+console.log(country[0]);
 
 console.log(country.length);
+
+country[2]="Bhootan";
+
+console.log(country[2]);
