9058350eaa018f47812d40f683a9e01ab051dfa9
Github Projects <barik011@gmail.com>
2022-04-12 09:47:28 +0530

    Array practice

--- 06/01array.js
+++ 06/01array.js
@@ -2,10 +2,20 @@ var country = ["India", "Russia","Nepal","Srilanka","Bangladesh"];
 
 var state = new Array("Bihar","Delhi","UP","Jharkhand");
 
-console.log(country[0]);
+//console.log(country[0]);
 
-console.log(country.length);
+//console.log(country.length);
 
 country[2]="Bhootan";
 
-console.log(country[2]);
+//console.log(country[2]);
+
+
+var user= ["Barique","barik011@gmail.com",37,true];
+
+console.log(user);
+user.pop();//Delete last element of Array
+console.log(user);
+
+user.unshift("Unique Id");//Add New element at first position (in 0 index)of Array
+console.log(user);
