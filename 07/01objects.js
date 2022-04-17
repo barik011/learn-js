8f436571875fc7c39d7a0b3eb68cd8450cc4c137
Github Projects <barik011@gmail.com>
2022-04-17 08:58:44 +0530

    Object in depth

--- 07/01objects.js
+++ 07/01objects.js
@@ -7,12 +7,31 @@
      pin:854301
  }
 
- console.log(users.designation);
+//  console.log(users.designation);
 
- console.table(users);
+//  console.table(users);
 
- users.age=37;
+//  users.age=37;
 
- console.table(users);
+//  console.table(users);
+
+var users = {
+    id:01,
+    name:"Barique",
+    role:"Admin",
+    loginCount:11,
+    courseList:[],
+    buyCourse:function (courseName){
+        this.courseList.push(courseName);
+    },
+    getCourseCount: function (){
+        return `${this.name} is enrolled and total number of courses is: ${this.courseList.length}`
+    }
+}
+
+users.buyCourse("React JS");
+users.buyCourse("Material UI");
+
+console.log(users.getCourseCount());
 
  
