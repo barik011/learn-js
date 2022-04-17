2ad74f99c9cfaa5dddea4425d2541ca3d482c572
Github Projects <barik011@gmail.com>
2022-04-17 08:30:58 +0530

    Objects

---
+++ 07/01objects.js
@@ -0,0 +1,15 @@
+ var users = {
+     id:01,
+     name:"Barique",
+     designation:"Frontend Developer",
+     age:35,
+     address:"Line Bazar",
+     pin:854301
+ }
+
+ console.log(users.designation);
+
+ console.table(users);
+
+
+ 
