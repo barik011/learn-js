2660275fab4a12ad0641ffa91eadefa12258f670
Github Projects <barik011@gmail.com>
2022-04-18 10:03:49 +0530

    looping

--- 07/02looping.js
+++ 07/02looping.js
@@ -2,13 +2,22 @@ var state = [
     "Bihar",
     "Jharkhand",
     "Up",
+    1994,
     "Delhi",
     "West Bengal",
     "Rajisthan",
     "Utrakhand"
 ];
 
-for(var i=0; i < state.length;i++){
-    console.log(state[i]);
+// for(var i=0; i < state.length;i++){
+//     console.log(state[i]);
+// }
+
+// for(var i=0; i < state.length;i++){
+//    if(typeof state[i]!=="string") break;
+//     console.log(state[i]);
+// }
+for(var i=0; i < state.length;i++) {
+    if(typeof state[i]!=="string") continue;
+     console.log(state[i]);
 }
-console.log(state);
